26434b586ccb8a973a5050164468bd05f188c4a1
Ali <mmoha055@ucr.edu>
2024-08-06 21:25:41 +0000

    Implement some methods as part of the component lifecycle

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,23 +7,40 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus : 'unknown' | 'offline' | 'online' = 'online';
 
+  private interval ?: ReturnType<typeof setInterval> ;
   constructor() {
-    setInterval(() => {
-    const rd = Math.random();
-      // if < 0.5  online
-      // else if  <0.9 offline
-      // else unknown
-      if (rd < 0.5) {
-        this.currentStatus = 'online';
-      } else if (rd < 0.9) {
-        this.currentStatus = 'offline';
-      } else {
-        this.currentStatus = 'unknown';
-      }
 
-    }, 2000);
   };
+
+  ngOnInit(): void {
+    console.log(" in ngOnInit");
+    
+    setInterval(() => {
+      const rd = Math.random();
+        // if < 0.5  online
+        // else if  <0.9 offline
+        // else unknown
+        if (rd < 0.5) {
+          this.currentStatus = 'online';
+        } else if (rd < 0.9) {
+          this.currentStatus = 'offline';
+        } else {
+          this.currentStatus = 'unknown';
+        }
+  
+      }, 2000);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(" in ngAfterViewInit");
+    
+  }
+
+  ngOnDestroy(): void {
+    console.log(" in ngOnDestroy");
+    clearInterval(this.interval);
+  }
 }
